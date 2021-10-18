99e19961dc4c2af4f411d84e727ff5ab9db873db
Sergio Navarrete <sergio.nava89@gmail.com>
2021-10-18 16:15:11 +0100

    Name replacement

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact Sergio Navarrete via email @ sergio.nava89@gmail.com"
   >
     <article className="post" id="contact">
       <header>
